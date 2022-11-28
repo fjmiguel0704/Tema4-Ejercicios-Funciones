9848984ba28d9f07040188679a42572713ee263f
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2022-11-28 13:43:21 +0100

    T4-Ejercicios-Funciones

--- Tema4-Ejercicios-Funciones/src/funciones/Ejercicio6.java
+++ Tema4-Ejercicios-Funciones/src/funciones/Ejercicio6.java
@@ -3,5 +3,26 @@ package funciones;
 import java.util.Scanner;
 
 public class Ejercicio6 {
+	public static void main(String[] args) {
+		int num;
+		
+		Scanner read = new Scanner (System.in);
+		System.out.println("Introduce un número: ");
+		num=read.nextInt();
+		
+		System.out.println(esPrimo(num));
+		
+	}
+	
+	static boolean esPrimo (int num) {
+		for(int i = 2; i<num; i++) {
+			if (num%i==0) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
 
 }
